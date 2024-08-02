3f38220a58ffb360cbc1ddca3ed9e98d7271dea1
alexpankov87 <deadwalkingday@gmail.com>
2024-08-02 12:58:23 +0600

    add

--- js/formsender.js
+++ js/formsender.js
@@ -1,51 +1,57 @@
-function submitForm() {
-    let form = document.getElementById('feedback-form');
+function submitForm(form) {
+            if (!form.checkValidity()) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Ошибка",
+                    text: "Пожалуйста, заполните все обязательные поля правильно.",
+                    confirmButtonColor: '#f24822',
+                    confirmButtonText: 'Окей'
+                });
+                return;
+            }
 
-    if (form.checkValidity()) {
-        let formData = new FormData(form);
+            let formData = new FormData(form);
 
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", form.action, true);
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", form.action, true);
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    let response = JSON.parse(xhr.responseText);
-                    if (response.status === "success") {
-                        // Успешный ответ от сервера, выполните дополнительные действия
-                        Swal.fire({
-                            icon: "success",
-                            title: "Успех",
-                            text: "Форма успешно отправлена!",
-                            confirmButtonColor: '#ec6e18',
-                            confirmButtonText: 'Окей'
-                        });
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        let response = JSON.parse(xhr.responseText);
+                        if (response.status === "success") {
+                            Swal.fire({
+                                icon: "success",
+                                title: "Успех",
+                                text: "Форма успешно отправлена!",
+                                confirmButtonColor: '#5fc65d',
+                                confirmButtonText: 'Окей'
+                            });
+                        } else {
+                            Swal.fire({
+                                icon: "error",
+                                title: "Ошибка",
+                                text: "Ошибка при отправке формы: " + response.message,
+                                confirmButtonColor: '#f24822',
+                                confirmButtonText: 'Окей'
+                            });
+                        }
                     } else {
-                        // Обработка ошибок
                         Swal.fire({
                             icon: "error",
                             title: "Ошибка",
-                            text: "Ошибка при отправке формы: " + response.message,
+                            text: "Ошибка при отправке формы. Пожалуйста, попробуйте позже.",
                             confirmButtonColor: '#f24822',
                             confirmButtonText: 'Окей'
                         });
                     }
-                } else {
-                    // Обработка ошибок
-                    Swal.fire({
-                        icon: "error",
-                        title: "Ошибка",
-                        text: "Ошибка при отправке формы. Пожалуйста, попробуйте позже.",
-                        confirmButtonColor: '#f24822',
-                        confirmButtonText: 'Окей'
-                    });
                 }
-            }
-        };
+            };
 
-        xhr.send(new URLSearchParams(formData));
-    } else {
-        form.reportValidity();
-    }
-}
+            let urlEncodedData = new URLSearchParams();
+            formData.forEach((value, key) => {
+                urlEncodedData.append(key, value);
+            });
+
+            xhr.send(urlEncodedData);
+        }
